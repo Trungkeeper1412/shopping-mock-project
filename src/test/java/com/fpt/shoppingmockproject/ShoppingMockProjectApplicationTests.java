3b258510ce6fecbfbb4ef9314f6b20dcbc374860
trungkeeper <trung14112002wweliker@gmail.com>
2024-06-17 09:05:11 +0700

    first commit

---
+++ src/test/java/com/fpt/shoppingmockproject/ShoppingMockProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.shoppingmockproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMockProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
